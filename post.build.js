2706edf2a2214481f1750d3fef4460610b72a1b1
spurushottam13 <formalspace@gmail.com>
2021-11-04 00:26:42 +0530

    [Build Pipeline]

--- post.build.js
+++ post.build.js
@@ -10,3 +10,12 @@ fs.copyFile(
     console.log("Copied Iframe File");
   }
 );
+
+fs.copyFile(
+  path.resolve(__dirname, "./public/mov_bbb.mp4"),
+  path.resolve(__dirname, "./dist/mov_bbb.mp4"),
+  (err) => {
+    if (err) throw err;
+    console.log("Copied Video File");
+  }
+);
